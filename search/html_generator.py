1a5e01bc4218a969dc2a33c7b81aa0da9ed01a85
faucac <faustocaceres1@gmail.com>
2023-05-23 18:00:48 -0300

    quitar span

--- search/html_generator.py
+++ search/html_generator.py
@@ -15,16 +15,15 @@ def generate_html(json_string):
 
         #   Title
         with a.h2(klass="wp-block-heading"):
-            a.span(
-                id=f"{song.split(' ')[1]}_{song_data['Track Name'].replace(' ', '_')}_8211_{song_data['Artist'].replace(' ', '_')}",
-                _t=f"{song.split(' ')[1]}. {song_data['Track Name']} &#8211;{song_data['Artist']}"
+            a(
+                f"{song.split(' ')[1]}. {song_data['Track Name']} &#8211;{song_data['Artist'].title()}"
             )
 
         #   Video
         with a.figure(klass="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"):
             with a.div(klass="wp-block-embed__wrapper"):
                 a.div(
-                    f'data-src="https://www.youtube.com/embed/{song_data["yt_video_id"]}" data-query="feature=oembed"', klass="rll-youtube-player")
+                    f'data-id="{song_data["yt_video_id"]}" data-src="https://www.youtube.com/embed/{song_data["yt_video_id"]}" data-query="feature=oembed"', klass="rll-youtube-player")
 
         #   Description
         a.p(_t=song_data["model_response"])
